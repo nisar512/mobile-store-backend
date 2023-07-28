e1ae892c766f1355c46984028f604fe920ba814f
Nisar Hussain <nisar.hussain@divedeepai.com>
2023-07-29 00:24:20 +0500

    connect to mongodb locally

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express=require("express");
+const mongoose=require("mongoose");
 const bodyParser=require("body-parser")
 const app= express();
 app.use(bodyParser.json());
-
-app.listen(8000);
+mongoose.connect('mongodb://127.0.0.1:27017/MOBILE').then(result => {
+    console.log("connected");   
+    app.listen(8000);
+}
+).catch(err => {
+    console.log(err);
+});
